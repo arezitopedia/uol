237b589bd6861a661289aceb9a25a98983c0b9df
Your Name <you@example.com>
2023-05-13 20:45:28 -0300

    OUL

---
+++ introduction_to_programming_1/sleuth/101/sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+    createCanvas(500, 500);
+}
+
+function draw() {
+    noStroke();
+    fill(255, 0, 0, 255);
+    rect(100, 100, 200, 200);
+    fill(0, 0, 255, 0);
+    rect(100, 100, 300, 300);
+}
